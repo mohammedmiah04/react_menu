b74e7a8b9b8d0433a222da2c3baedf62fdccbaea
MohammedM <nanumiah@nanus-mbp.home>
2021-01-20 11:25:07 +0000

    Updates

---
+++ precache-manifest.59b0720a331d2f9fc3792837cdef6213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a129683a0434ed0d6018e1551d6d40e2",
+    "url": "/react_menu/index.html"
+  },
+  {
+    "revision": "9e487032064bbd25fecd",
+    "url": "/react_menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "b97e677205dc2874d01e",
+    "url": "/react_menu/static/js/2.eef18229.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_menu/static/js/2.eef18229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e487032064bbd25fecd",
+    "url": "/react_menu/static/js/main.8b8a1401.chunk.js"
+  },
+  {
+    "revision": "35cc9736b88b8301b2f5",
+    "url": "/react_menu/static/js/runtime-main.0ebed204.js"
+  }
+]);
